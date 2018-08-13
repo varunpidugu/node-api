e2a57ab108c0a3b7317fbfa2e09a858b51a10ce5
varunpidugu <varun.pidugu@yahoo.com>
2018-08-13 13:43:05 +0530

    Added route for fetching Todos by ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,19 +32,35 @@ app.get('/todos', (req, res) => {
 
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
 
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+	}
 
-app.post('/users', (req, res) => {
-	var user = new User({
-		email: req.body.email
-	});
+	Todo.findById(id).then((todo) => {
+		if (!todo) return res.status(404).send();
 
-	user.save().then((doc) => {
-		res.send(doc);
-	}, (e) => {
-		res.status(400).send(e);
+		res.send({todo});
+
+	}).catch((e) => {
+		res.status(400).send();
 	})
-})
+	//validate ID
+});
+
+// app.post('/users', (req, res) => {
+// 	var user = new User({
+// 		email: req.body.email
+// 	});
+
+// 	user.save().then((doc) => {
+// 		res.send(doc);
+// 	}, (e) => {
+// 		res.status(400).send(e);
+// 	})
+// })
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
