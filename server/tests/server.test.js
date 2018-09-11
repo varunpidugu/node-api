d0b6dea0d275784c2b5385bd8444d0c626dc98bf
varunpidugu <varun.pidugu@yahoo.com>
2018-09-11 18:29:33 +0530

    New tests added

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,37 @@ describe('GET /todos/:id', () => {
 			.end(done)
 	})
 });
+
+describe('DELETE /todos/:id', () => {
+
+	it('should delete a todo', (done) => {
+
+		request(app)
+			.delete(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end((err, res) => {
+				if (err) return done(err);
+				Todo.findById(`${todos[0]._id.toHexString()}`).then((todo) => {
+					expect(todo).toBeFalsy();
+					done();
+				}).catch((e) => done(e));
+			})
+	})
+
+	it('should return 404 if todo not found', (done) => {
+		request(app)
+			.delete(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done)
+	});
+
+	it('should return 404 if invalid ObjectID', (done) => {
+		request(app)
+			.delete('/todos/123')
+			.expect(404)
+			.end(done)
+	})
+})
